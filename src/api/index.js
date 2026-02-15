2a92b62507f4958fb2fda7f1d519be20222ea41d
hanseroland <rolandntougou@gmail.com>
2026-02-15 17:31:54 +0000

    process.env.REACT_APP_API_BASE_URL -> import.meta.env.VITE_API_BASE_URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 //src/api/index.js
 import axios from "axios";
 
-//const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
-const API_BASE_URL = "http://localhost:5000";
+//const API_BASE_URL = "http://localhost:5000";
 
 
 export const axiosInstance = async (method, url, payload) => {
