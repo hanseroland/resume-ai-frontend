092f4d86870f5ea875ebc353f0e243801c24c525
hanseroland <rolandntougou@gmail.com>
2026-04-21 14:28:00 +0000

    chore(main): initialize QueryClientProvider and React Query devtools

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1, // Nombre de tentatives en cas d'échec
+            refetchOnWindowFocus: false, // Évite de re-fetch quand on change d'onglet
+        },
+    },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+          <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
